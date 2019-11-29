975f8fd1dba66bcd2c0fc5a124f15e0e39519199
unknown <Digital@DH-A10-10.dhbrasil.digitalhouse.com>
2019-11-29 11:33:46 -0300

    continuando novamente

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Product;
+use Auth;
 
 class ProductController extends Controller{
-    
+    public $nome;
     public function create(Request $request){
+        
+        $newProduct = new Product();
+        $newProduct->name = $request->nameProduct;
+        $newProduct->description = $request->descriptionProduct;
+        $newProduct->quantity = $request->quantityProduct;
+        $newProduct->price = $request->priceProduct;
+        $newProduct->description = $request->descriptionProduct;
+        $newProduct->user_id = Auth::user()->id;
+
+        $result = $newProduct->save();
+
+        if($result){
+            echo "Deu certo query!";
+
+        }else{
+            echo "Vai ter que criar hein!";
+        }
+
+
+
+
+
+
         
         //o metodo create fica responsavel por cadastrar o porduto.
         /*if($request->isMethod('GET')){
@@ -16,7 +41,7 @@ class ProductController extends Controller{
         }else{
 
         }*/
-        dd($request->nameProduct);
+        //dd($request->nameProduct);
     }
 
     public function viewForm(Request $request){
